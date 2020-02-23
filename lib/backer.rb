1c933ee445a8a81a58e6b8c4aaec55a104aa5823
Samantha Meason <sbmeason@gmail.com>
2020-02-23 23:50:35 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,5 +7,8 @@ class Backer
     @backed_projects = []
   end
   
-  
+  def back_project(project)
+    @backed_projects << project
+    project.add_backer(self) unless project.backers.include?(self)
+  end
 end
