78d62268291105e468305cf999824b581865479f
Samantha Meason <sbmeason@gmail.com>
2020-02-23 23:50:29 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,11 @@
 class Project
-  attr_accessor :backed_projects
+  attr_accessor :backers
   attr_reader :title
   
   def initialize(title)
     @title = title
-    @backed_projects = []
+    @backers = []
   end
   
-  def back_project(project)
-    @backed_projects << project
-    project.add_backer(self) unless project.backers.include?(self)
-  end
+  
 end
